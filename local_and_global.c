fa56c68ecd8b598b25fffcea2d588cd10a07748f
Emilio Sebastian Daza Vigo <emiliodaza@Emilios-MacBook-Air.local>
2026-02-18 00:00:44 -0500

    New Progress

--- local_and_global.c
+++ local_and_global.c
@@ -3,11 +3,19 @@
 #include <llvm-c/Core.h>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
 
 bool run_common_subexpression_elimination(LLVMBasicBlockRef bb);
 bool run_constant_folding(LLVMBasicBlockRef bb);
 bool run_dead_code_elimination(LLVMValueRef func);
 bool is_store_in_between_shared_memory_uses(LLVMValueRef first_instruction, LLVMValueRef second_instruction);
+std::unordered_set <LLVMValueRef> compute_gen_set_for_block(LLVMBasicBlockRef bb);
+std::unordered_set <LLVMValueRef> set_of_all_store (LLVMValueRef func);
+std::unordered_set <LLVMValueRef> compute_kill_set_for_block(LLVMBasicBlockRef bb);
+struct IN_and_OUT {
+    std::unordered_set <LLVMValueRef> in_set;
+    std::unordered_set <LLVMValueRef> out_set;
+} IN_and_OUT;
 
 int main(){
 
@@ -191,3 +199,109 @@ std::unordered_set <LLVMValueRef> compute_gen_set_for_block(LLVMBasicBlockRef bb
     }
     return block_gen_set;
 } 
+
+// computing the set of all store instructions in the function
+std::unordered_set <LLVMValueRef> set_of_all_store (LLVMValueRef func) {
+    std::unordered_set <LLVMValueRef> set_of_all_store = {};
+    for (LLVMBasicBlockRef bb = LLVMGetFirstBasicBlock(func); bb != NULL; bb = LLVMGetNextBasicBlock(bb)){
+        for (LLVMValueRef ins = LLVMGetFirstInstruction(bb); ins != NULL; ins = LLVMGetNextInstruction(ins)){
+            LLVMOpcode type_of_ins = LLVMGetInstructionOpcode(ins);
+            if (type_of_ins == LLVMStore) {
+                set_of_all_store.insert(ins);
+            }
+        }
+    }
+}
+
+// computing the set KILL[B] for a basic block B
+std::unordered_set <LLVMValueRef> compute_kill_set_for_block(LLVMBasicBlockRef bb) {
+
+    std::unordered_set <LLVMValueRef> block_kill_set = {};
+
+    // getting the function to determine the set of all stores
+    LLVMValueRef func = LLVMGetBasicBlockParent(bb);
+    std::unordered_set <LLVMValueRef> set_of_stores = set_of_all_store(func);
+
+    for (LLVMValueRef ins = LLVMGetFirstInstruction(bb); ins != NULL; ins = LLVMGetNextInstruction(ins)){
+        if (LLVMGetInstructionOpcode(ins) == LLVMStore){
+            for (LLVMValueRef store_ins : set_of_stores) {
+                if (store_ins != ins) { // so that all the others are added
+                    block_kill_set.insert(store_ins);
+                }
+            }
+        }
+    }
+}
+
+// Getting the predecessors map where the keys are the blocks and the values are their corresponding predecessors
+std::unordered_map <LLVMBasicBlockRef, std::unordered_set <LLVMBasicBlockRef>> compute_predecesor_blocks (LLVMValueRef func) {
+    std::unordered_map <LLVMBasicBlockRef, std::unordered_set <LLVMBasicBlockRef>> predecessors_map;
+
+    for (LLVMBasicBlockRef bb = LLVMGetFirstBasicBlock(func); bb != NULL; bb = LLVMGetNextBasicBlock(bb)) {
+        LLVMValueRef terminator = LLVMGetBasicBlockTerminator(bb); // getting the terminator to access the successors
+        if (terminator == NULL) {
+            continue;
+        }
+        unsigned successor_index = 0;
+        while (successor_index < LLVMGetNumSuccessors(terminator)) {
+            LLVMBasicBlockRef successor = LLVMGetSuccessor(terminator, successor_index);
+            predecessors_map[successor].insert(bb); // because the successor's predecessor is the current block
+            successor_index++;
+        }
+    }
+    return predecessors_map;
+}
+
+std::unordered_map <LLVMBasicBlockRef, IN_and_OUT> in_and_out_sets_map(LLVMvalueRef func) {
+    // we begin by computing the predecessors for each block for easier later computation
+    std::unordered_map <LLVMBasicBlockRef, std::unordered_set <LLVMBasicBlockRef>> predecessors_map = compute_predecesor_blocks(func);
+    // initializing each "in set" as empty and also initializing each "out set" as the gen set
+    std::unordered_map <LLVMBasicBlockRef, IN_and_OUT> in_and_out_sets_map;
+    for (LLVMBasicBlockRef bb = LLVMGetFirstBasicBlock(func);
+        bb != NULL;
+        bb = LLVMGetNextBasicBlock(bb)) {
+            std::unordered_set <LLVMValueRef> in_set = {};
+            std::unordered_set <LLVMValueRef> out_set = compute_gen_set_for_block(bb); 
+
+            IN_and_OUT IN_and_OUT_element;
+            // we provide the in_set and out_set data to the IN_and_OUT_element
+            IN_and_OUT_element.in_set_in_block = in_set;
+            IN_and_OUT_element.out_set_in_block = out_set;
+
+            in_and_out_sets_map[bb] = IN_and_OUT_element;
+        } 
+
+    bool change = true; // in order to stop when we have reached a fixed point
+    while (change) {
+        change = false; // if a change is found this will change to true again
+        for (LLVMBasicBlockRef bb = LLVMGetFirstBasicBlock(func);
+            bb != NULL;
+            bb = LLVMGetNextBasicBlock(bb)) {
+                std::unordered_set <LLVMBasicBlockRef> predecessors_of_bb = predecessors_map[bb];
+                // computing and storing IN[B] as the union of the predecessors OUT
+                for (LLVMBasicBlockRef predecessor : predecessors_of_bb) {
+                    in_and_out_sets_map[bb].in_set_in_block.insert(in_and_out_sets_map[predecessor].out_set_in_block.begin(), in_and_out_sets_map[predecessor].out_set_in_block.end());
+                }
+                // Storing the old OUT of the block to compare with the new one and see if change has ocurred
+                std::unordered_set <LLVMValueRef> old_out = in_and_out_sets_map[bb].out_set_in_block;
+                // we compute the gen set of the block to do OUT[B] = GEN[B] union (in[B] - kill[B])
+                std::unordered_set <LLVMValueRef> gen_set_of_block = compute_gen_set_for_block(bb);
+                // first we include all the gen set elements associated
+                in_and_out_sets_map[bb].out_set_in_block.insert(gen_set_of_block.begin(), gen_set_of_block.end());
+                // inserting IN[B] - KILL[B] to finish defining OUT[B]
+                std::unordered_set <LLVMValueRef> in_instructions = in_and_out_sets_map[bb].in_set_in_block;
+                for (LLVMValueRef IN_instruction : in_instructions) {
+                    std::unordered_set <LLVMValueRef> kill_instructions_for_block = compute_kill_set_for_block(bb);
+                    for (LLVMValueRef KILL_instruction : kill_instructions_for_block) {
+                        if (IN_instruction != KILL_instruction) {
+                            in_and_out_sets_map[bb].out_set_in_block.insert(IN_instruction);
+                        }
+                    }
+                }
+                // checking if a change has occured
+                if (in_and_out_sets_map[bb].out_set_in_block != old_out) {
+                    change = true;
+                }
+            }
+    }
+}
